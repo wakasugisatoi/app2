7761fbd9c4eea38a276e27df45b230b681d8badc
WakasugiSatoi <wakasugi.sa101@gmail.com>
2023-11-11 16:23:04 +0000

    コメント非同期作成中

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,12 +5,13 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
-    redirect_to post_image_path(post_image)
+    # redirect_to post_image_path(post_image)
   end
   
   def destroy
+    # @post_image = PostImage.find(params[:post_image_id])
     PostComment.find(params[:id]).destroy
-    redirect_to post_image_path(params[:post_image_id])
+    # redirect_to post_image_path(params[:post_image_id])
   end
     
 
